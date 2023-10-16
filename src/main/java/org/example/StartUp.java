cf7bed4393a3f16566ee04a2a0033350e68e0641
daro-decagon <gadibiao@decagonhq.com>
2023-10-16 13:58:48 +0100

    initial commit

--- src/main/java/org/example/StartUp.java
+++ src/main/java/org/example/StartUp.java
@@ -7,24 +7,28 @@ public class StartUp {
 //       User adamu = new User();
 //        adamu.userCourseAndMedia();
 
+        String[] arrayOfWords = {"HUMAN", "CAT", "DOG", "METACOGNITION", "MULTI-DIMENSIONAL", "XYZ"};
 
-        int age = 35;
-
-        boolean condition = age < 13;
-        boolean condition2 = age > 18;
-        String adultContent = condition?"Turn off adult content":(condition2?"MA allow mature audiences":"Watch to your heart's content!");
-
-        System.out.println(adultContent);
-//        String adultContent;
-        if (condition){
-            adultContent = "Turn off adult content";
-            System.out.println(adultContent);
-        } else if (condition2) {
-            adultContent = "MA allow mature audiences";
-            System.out.println(adultContent);
-        } else {
-            adultContent = "Watch to your heart's content!";
-            System.out.println(adultContent);
+        for (int i = 0; i < arrayOfWords.length; i++) {
+            System.out.println("Index: "+ i +" Element: "+ arrayOfWords[i]);
         }
+//        int age = 35;
+//
+//        boolean condition = age < 13;
+//        boolean condition2 = age > 18;
+//        String adultContent = condition?"Turn off adult content":(condition2?"MA allow mature audiences":"Watch to your heart's content!");
+//
+//        System.out.println(adultContent);
+////        String adultContent;
+//        if (condition){
+//            adultContent = "Turn off adult content";
+//            System.out.println(adultContent);
+//        } else if (condition2) {
+//            adultContent = "MA allow mature audiences";
+//            System.out.println(adultContent);
+//        } else {
+//            adultContent = "Watch to your heart's content!";
+//            System.out.println(adultContent);
+//        }
     }
 }
