9f76922047d50176cd7a9c437a7e8d7c655b2c49
daro-decagon <gadibiao@decagonhq.com>
2023-10-16 14:55:53 +0100

    duplicate algorithm

--- src/main/java/org/example/StartUp.java
+++ src/main/java/org/example/StartUp.java
@@ -2,15 +2,38 @@ package org.example;
 
 import org.example.entities.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StartUp {
     public static void main(String[] args) {
 //       User adamu = new User();
 //        adamu.userCourseAndMedia();
 
-        String[] arrayOfWords = {"HUMAN", "CAT", "DOG", "METACOGNITION", "MULTI-DIMENSIONAL", "XYZ"};
+        String[][] arrayOfWords = {new String[]{"HUMAN", "CAT", "DOG", "METACOGNITION", "MULTI-DIMENSIONAL", "XYZ"}, {"DEPARTMENT", "JUDICIARY", "LEGISLATIVE"}};
 
+        System.out.println("LENGTH OF ARRAY : "+arrayOfWords.length);
         for (int i = 0; i < arrayOfWords.length; i++) {
-            System.out.println("Index: "+ i +" Element: "+ arrayOfWords[i]);
+            for (int j = 0; j < arrayOfWords[i].length; j++) {
+                System.out.println("Array Index: "+ i +" Array Element: "+ arrayOfWords[i][j]);
+            }
+        }
+
+        int[] numbers = {1, 3, 5, 8, 5, 6, 7, 3};
+        List<Integer> duplicateArray = new ArrayList<>();
+        boolean isDuplicated  = false;
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                System.out.println("Current number: "+numbers[i] + " ---- Checking number: "+numbers[j]);
+                if (numbers[i]==numbers[j]){
+                    duplicateArray.add(numbers[i]);
+                    isDuplicated = true;
+                }
+            }
+
+        }
+        System.out.println("DUPLICATED: "+isDuplicated + " DUPLICATE(S) : "+duplicateArray);
+
         }
 //        int age = 35;
 //
@@ -31,4 +54,3 @@ public class StartUp {
 //            System.out.println(adultContent);
 //        }
     }
-}
